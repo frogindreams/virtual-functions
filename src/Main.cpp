abb15d05b436a59e0d13be70bd63f590eba1dba9
frogindreams <frogharvard@gmail.com>
2022-11-26 23:36:19 +0700

    add private generator & change the default constructor

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,28 +1,52 @@
 #include <iostream>
+#include <cstdlib>
 
 class CStr
 {
-    private:
-        int value;
+    char *string = NULL;
+
+    char *generateRandomString(int lengthOfString)
+    {
+        srand(time(0));
+        char letters[] = "abcdefghijklmnopqrstuvwxyz";
+
+        string = new char[lengthOfString]; 
+        string[lengthOfString] = { 0 };
+
+        for (int idx = 0; idx < lengthOfString; idx++)
+        {
+            string[idx] = letters[rand() % 26];
+        }
+
+        return string;
+    }
+
+    int generateRandomNumber()
+    {
+        srand(time(0));
+
+        return rand() % 20;
+    }
 
     public:
-        CStr(int value)
+        CStr() 
         {
-            this -> value = value;
+            int lengthOfString = generateRandomNumber();
+
+            generateRandomString( lengthOfString );
         }
 
-        int getValue()
+        char *getString()
         {
-            return value;
+            return string;
         }
-        
 };
 
 int main()
 {
-    CStr object(55);
+    CStr string;
 
-    std::cout << object.getValue() << "\n";
+    std::cout << string.getString() << "\n";
 
     return 0;
 }

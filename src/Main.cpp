f96807d5b62e5bd102d89ab78a8f8fa09c750851
frogindreams <frogharvard@gmail.com>
2022-11-28 20:27:57 +0700

    add operators

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 #include <cstdlib>
+#include <string.h>
+
+using std::cout;
 
 class CStr
 { 
-    char *string = NULL;
+    char *string = nullptr;
+    int lengthOfString;
 
     char *generateRandomString(int lengthOfString)
     {
         srand(time(0));
-        char letters[] = "abcdefghijklmnopqrstuvwxyz0";
+        const char letters[] = "abcdefghijklmnopqrstuvwxyz0";
 
-        string = new char[lengthOfString]; 
-        string[lengthOfString] = { 0 };
+        this -> string = new char[lengthOfString + 1]; 
+        string[lengthOfString] = '\0';
 
         for (int idx = 0; idx < lengthOfString; idx++)
         {
@@ -25,56 +29,123 @@ class CStr
     {
         srand(time(0));
 
-        return rand() % 20;
+        return 1 + rand() % 19;
     }
 
     public:
         CStr() 
         {
-            int lengthOfString = generateRandomNumber();
+            this -> lengthOfString = generateRandomNumber();
 
             generateRandomString( lengthOfString );
         }
 
-        CStr(int lengthOfString)
+        CStr(char *string)
         {
-            if ( lengthOfString <= 20 )
-                generateRandomString( lengthOfString );
-        }
+            this -> lengthOfString = strlen(string);
 
-        CStr(char *string[])
-        {
-            size_t lengthOfString = (unsigned) sizeof &string;
-
-            if ( string[lengthOfString] == 0 )
+            if ( string[lengthOfString] == '\0' )
             {
                 this -> string = (char *) string; 
             }
         }
 
+        CStr(int lengthOfString)
+        {
+            this -> lengthOfString = lengthOfString;
+
+            if ( lengthOfString <= 20 )
+            {
+                generateRandomString( lengthOfString );
+            }
+        }
+
         CStr(const CStr &refer)
         {
-            this -> string = (char *) refer.string;
+            this -> string = new char[refer.lengthOfString];
+
+            for (int idx = 0; idx < refer.lengthOfString; idx++)
+            {
+                this -> string[idx] = refer.string[idx];
+            }
+
+            this -> lengthOfString = refer.lengthOfString;
+        }
+
+        ~CStr()
+        {
+            delete[] string;
+        }
+
+        CStr& operator = (const CStr &object)
+        {
+            if (this == &object)
+            {
+                return *this;
+            }
+
+            int objLength = object.lengthOfString;
+            this -> lengthOfString = objLength;
+
+            if (this -> string != nullptr)
+            {
+                delete[] this -> string;
+            }
+
+            this -> string = new char[objLength];
+
+            for (int idx = 0; idx < objLength; idx++)
+            {
+                this -> string[idx] = object.string[idx]; 
+            }
+
+            return *this;
+        }
+
+        CStr& operator = (const char *string)
+        {
+            if (this -> string == string) 
+            {
+                return *this;
+            }
+
+            int lengthOfAnotherString = strlen(string);
+            this -> lengthOfString = lengthOfAnotherString;
+
+            if (this -> string != nullptr)
+            {
+                delete[] this -> string;
+            }
+
+            this -> string = new char[lengthOfAnotherString];
+
+            for (int idx = 0; idx < lengthOfAnotherString; idx++)
+            {
+                this -> string[idx] = string[idx]; 
+            }
+
+            return *this;
         }
 
         char *getString()
         {
             return string;
         }
+
+        int getLengthOfString()
+        {
+            return lengthOfString;
+        }
 };
 
 int main()
-{
-    CStr defaultString;
-    CStr generatedStringOnDefault( defaultString );
-    CStr generatedStringLength( 20 );
-    CStr copiedString = defaultString;
+{    
+    CStr randomString;
+    CStr stringLength(14);
+    randomString = stringLength.getString();
 
-
-    std::cout << "Default String: " << defaultString.getString() << "\n";
-    std::cout << "Generated String on default: " << generatedStringOnDefault.getString() << "\n";
-    std::cout << "Generated String with given length: " << generatedStringLength.getString() << "\n";
-    std::cout << "Copied String from default: " << copiedString.getString() << "\n";
+    cout << randomString.getString() << "\n";
+    cout << stringLength.getString() << "\n";
 
     return 0;
 }

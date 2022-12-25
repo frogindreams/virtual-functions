fdbd2d619b8cd2e2c8a72c54d278058181d27999
frogindreams <frogharvard@gmail.com>
2022-12-25 15:30:47 +0700

    Changed SFile, SQueue
    Signed-off-by: frogindreams <frogharvard@gmail.com>

--- Main.cpp
+++ Main.cpp
@@ -14,49 +14,13 @@ class SBase
     private:
 };
 
-class SFile : SBase 
-{
-    public:
-        SFile(const char* filename)
-        {
-            this -> filename = filename;
-        }
-
-        int get() override
-        {
-            try 
-            {
-                FILE* fileOpened = fopen(filename, "r");
-
-                int character;
-                while ((character = fgetc(fileOpened)) != EOF) 
-                {
-                    putwchar(character);
-                }
-
-                fclose(fileOpened);
-                return -1;
-            }
-
-            catch (const std::exception& e) 
-            {
-                std::cout << e.what() << "\n";
-                return 0;
-            }
-        }
-
-    private:
-        const char* filename = nullptr;
-};
-
-class SKbrd : SBase 
+class SKbrd : public SBase 
 {
     public:
         int get() override
         {
-            std::string lineOfNumbers = "";
-
-            getline(std::cin, lineOfNumbers);
+            int inputNumber;
+            std::cin >> inputNumber;
 
             return -1;
         }
@@ -64,32 +28,104 @@ class SKbrd : SBase
     private:
 };
 
-class SQueue : SBase
+class SFile : public SBase 
 {
+    public:
+        int get() override
+        {
+            int isOk = 0;
+            char inputNumber[255];
+            const char* filename = "file.txt";
+            FILE* openedFile = fopen(filename, "r+");
+
+            if (!openedFile) 
+            {
+                perror("File opening failed");
+                return isOk;
+            }
+
+            printf("Enter your number for file: ");
+            fgets(inputNumber, 255, stdin);
+            fputs(inputNumber, openedFile);
+            rewind(openedFile);
+
+            int ch;
+            while ((ch = fgetc(openedFile)) != EOF) 
+            {
+                putchar(ch);
+            }
+
+            if (ferror(openedFile)) 
+            {
+                puts("I/O error when reading");
+            } 
+            
+            else if (feof(openedFile))
+            {
+                isOk = -1;
+            }
+
+            fclose(openedFile);
+            return isOk;
+        }
+
+    private:
+};
+
+class SQueue : public SBase
+{
+    int *queue, sizeOfQueue;
+
+    void initQueue() 
+    {
+        queue = new int[sizeOfQueue];
+        for (int idx = 0; idx < sizeOfQueue - 1; idx++) 
+        {
+            queue[idx] = rand() % 100;
+        }
+    }
+
     public:
         SQueue(int sizeOfQueue) 
         {
             this -> sizeOfQueue = sizeOfQueue;
         }
 
+        ~SQueue() 
+        {
+            delete[] queue;
+        }
+
         int get() override
         {
-            std::queue<int> queueOfNumbers;
+            initQueue();
 
-            while (queueOfNumbers.size() != sizeOfQueue) 
-            {
-                queueOfNumbers.push( rand() % 100 );
-            }
+            int inputNumber;
+            printf("Enter your number for queue: ");
+            std::cin >> inputNumber;
+
+            queue[sizeOfQueue - 1] = inputNumber;
 
             return -1;
         }
 
-    private:
-        int sizeOfQueue;
+        void showQueue() 
+        {
+            for (int idx = 0; idx < sizeOfQueue; idx++) 
+            {
+                std::cout << queue[idx] << " ";
+            }
+
+            std::cout << "\n";
+        }
 };
 
 int main() {
     srand(time(0));
+
+    SQueue queue(5);
+    queue.get();
+    queue.showQueue();
 
     return 0;
 }
